6c2440b2f483d24a3a536ba1daf86e337691804f
Victor Aborisade <112784241+Aborisade@users.noreply.github.com>
2025-11-06 19:03:21 +0100

    Email worked on

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,9 +2,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
